309c58b7d04e46afcf409ca46cad1101bc834801
Tiago Lopes <tiagojavascript@gmail.com>
2017-10-01 23:15:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,7 +15,7 @@ function whileLoop(n){
 function doWhileLoop(array){
   function maybeTrue() {
     return Math.random() >= 0.5
-  };
+  }
   do{
     array.pop();
   } while (array.length > 0 && maybeTrue());
